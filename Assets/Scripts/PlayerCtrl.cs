f6d79ef83d8a4fa660a0621a7a197982e7ae000b
Michael Williams <mikewilliams287@gmail.com>
2024-07-20 20:16:05 -0700

    Lesson 32, 33 - Animation
    -Created idle and run animations
    -Created animation transitions
    -Added to player script to trigger animation transtions

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -6,19 +6,45 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
     public float playerSpeed;
+    private float input;
     Rigidbody2D rb;
+    Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        if (input != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (input > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (input < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float input = Input.GetAxisRaw("Horizontal");
+        input = Input.GetAxisRaw("Horizontal");
         //print(input);
 
         rb.velocity = new Vector2(input * playerSpeed, rb.velocity.y);
