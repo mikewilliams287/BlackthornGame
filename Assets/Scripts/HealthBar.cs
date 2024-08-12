a3630e06f140cc1ae71c74d90012e1cc4cce0282
Michael Williams <mikewilliams287@gmail.com>
2024-08-12 12:21:57 -0700

    Hazard Spawner & health bar
    -Created new hazard spawner
    -Added score tracking and final score display
    -Added amount and color lerp to health bar
    -added heart icon

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,21 +6,66 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 
 {
+    [SerializeField] private float _timeToDrain = 0.25f;
+    [SerializeField] private Gradient _healthBarGradient;
     private Image _image;
+
+    private float _targetFillAmount = 1;
+
+    private Color _newHealthBarColor;
+
+    private Coroutine drainHealthBarCoroutine;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _image = GetComponent<Image>();
 
+        // set the starting health bar color
+        _image.color = _healthBarGradient.Evaluate(_targetFillAmount);
+
+        CheckHealthBarGradientAmount();
+
     }
 
     public void UpdateHealthBar(float startHealth, float currentHealth)
     {
-        print("ITS WORKING");
-        print(currentHealth);
-        _image.fillAmount = currentHealth / startHealth;
+        //print("ITS WORKING");
+        //print(currentHealth);
+        _targetFillAmount = currentHealth / startHealth;
 
+        drainHealthBarCoroutine = StartCoroutine(DrainHealthBar());
+
+        CheckHealthBarGradientAmount();
+
+    }
+
+    private IEnumerator DrainHealthBar()
+    {
+        float fillAmount = _image.fillAmount;
+        Color currentColor = _image.color;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < _timeToDrain)
+        {
+            elapsedTime += Time.deltaTime;
+
+            // Lerp the fill amount
+            _image.fillAmount = Mathf.Lerp(fillAmount, _targetFillAmount, (elapsedTime / _timeToDrain));
+
+            // Lerp the fill color
+            _image.color = Color.Lerp(currentColor, _newHealthBarColor, (elapsedTime / _timeToDrain));
+
+
+            yield return null;
+        }
+
+    }
+
+    private void CheckHealthBarGradientAmount()
+    {
+        _newHealthBarColor = _healthBarGradient.Evaluate(_targetFillAmount);
     }
 
     // Update is called once per frame
