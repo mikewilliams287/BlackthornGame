0f5477400077006f97466ec271c8f24a8b8663ca
Michael Williams <mikewilliams287@gmail.com>
2024-10-06 11:56:02 -0700

    Fixed some functionality and UI issues

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,10 +8,28 @@ public class GameManager : MonoBehaviour
 
     public void PlayGame()
     {
-        AudioManager.instance.PlayMusic(null);
-        AudioManager.instance.musicSource.Play();
+        //AudioManager.instance.PlayMusic(null);
+        //AudioManager.instance.musicSource.Play();
         SceneManager.LoadScene("Game");
     }
+
+    public void RestartGame()
+    {
+        // Reload the current active scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        //This will close the game when built as an executable
+        Application.Quit();
+
+        //This will stop the play mode in the Unity Editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
 
     // Start is called before the first frame update
     void Start()
