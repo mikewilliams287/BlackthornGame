9e6f6e85b7540149870e296e26460395dfb5432a
Michael Williams <mikewilliams287@gmail.com>
2025-01-18 11:20:06 -0800

    Added collectable item
    -Created collectable gem prefab
    -Created Collectable Interface script
    -Created script to update Gem count UI when gem collected

---
+++ Assets/Scripts/Collectibles/GemUIText.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GemUIText : MonoBehaviour
+
+
+{
+    // The current gem count
+    int gemCount;
+
+    // Reference to the TextMeshPro component
+    public TextMeshProUGUI gemsText;
+
+    // Subscribe to the event when game object is enabled
+    private void OnEnable()
+    {
+        Gem.OnGemCollected += IncrementGemCount;
+    }
+
+    //Unsubscribe when game object disabled
+    private void OnDisable()
+    {
+        Gem.OnGemCollected -= IncrementGemCount;
+    }
+
+    private void Start()
+    {
+        // Get the TextMeshProIGUI component attached to this GameObject
+        gemsText = GetComponent<TextMeshProUGUI>();
+
+        // Initialize the gem count and update the UI
+        gemCount = 0;
+        UpdateGemText();
+    }
+
+    // Method to increment gem count
+    public void IncrementGemCount()
+    {
+        // Increment gem count by 1
+        gemCount++;
+
+        // Update the TextMeshPro UI text to refelct new gem count
+        UpdateGemText();
+    }
+
+    // Private "helper" method to update the UI text
+    private void UpdateGemText()
+    {
+        // set the TextMeshPro text to display the gem count
+        gemsText.text = gemCount.ToString();
+    }
+
+}
